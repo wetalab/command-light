cc2ff8da7b942dbaa4960e0cfa15b63a97ab1b83
曹操 <xuzhao.sgn@gmail.com>
2023-04-26 21:35:33 +0800

    use storybook instead of

---
+++ src/components/Command.utils.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+
+type CommandPrimitiveElement = React.ElementRef<typeof CommandPrimitive>
+type CommandPrimitiveProps = React.ComponentPropsWithoutRef<typeof CommandPrimitive>
--- src/components/Commandot.tsx
+++ src/components/Commandot.tsx
@@ -3,17 +3,20 @@ import * as Dialog from '@radix-ui/react-dialog';
 
 export interface CommandotProps {
   isOpen: boolean
+  title: string
+  placeHolder: string
   search: string
   pages: string[]
 
 }
 
+const Commandot = ({
 
-const Commandot = () =>{
+}) =>{
 
   return(
     <div>
-      <Dialog.Root>
+      <Dialog.Root open={isOpen}>
         <Dialog.Trigger asChild>
           <button className="text-violet11 shadow-blackA7 hover:bg-mauve3 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-white px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none">
             Edit profile
@@ -22,18 +25,9 @@ const Commandot = () =>{
         <Dialog.Portal>
           <Dialog.Overlay className="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0" />
           <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
-            <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
-              Edit profile
-            </Dialog.Title>
-            <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
-              Make changes to your profile here. Click save when you're done.
-            </Dialog.Description>
             <fieldset className="mb-[15px] flex items-center gap-5">
-              <label className="text-violet11 w-[90px] text-right text-[15px]" htmlFor="name">
-                Name
-              </label>
               <input
-                className="text-violet11 shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
+                className="bg-white text-black "
                 id="name"
                 defaultValue="Pedro Duarte"
               />
